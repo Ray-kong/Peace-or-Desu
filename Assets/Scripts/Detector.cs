2ba9504e32a1abd9e215fbde638bcdced25666e8
RayKong <ray.hyun.kong@gmail.com>
2023-07-29 18:44:18 +0900

    fucking hell

--- Assets/Scripts/Detector.cs
+++ Assets/Scripts/Detector.cs
@@ -1,26 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Detector : MonoBehaviour
 {
-    // Define the tag of the object you want to detect
-    [SerializeField] private string objectTag = "NPC";
+    [SerializeField] private string objectTag = "Player";
     [HideInInspector] public bool isWithinCollider;
-    
-
-    // void OnTriggerEnter2D(Collider2D other)
-    // {
-    //     if (other.gameObject.CompareTag(objectTag))
-    //     {
-    //         Debug.Log("Object with tag " + objectTag + " entered the box collider.");
-    //     }
-    // }
 
     void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.CompareTag(objectTag))
         {
+            Debug.Log("Object with tag " + objectTag + " is within the box collider.");
             isWithinCollider = true;
         }
     }
@@ -47,6 +36,4 @@ public class Detector : MonoBehaviour
             }
         }
     }
-
-
 }
