d348fc26eb051d7c7f1fd2182d6b1d57db176b97
Keito605 <82559995+Keito605@users.noreply.github.com>
2023-07-30 03:48:34 +0900

    menenen

--- Assets/Scripts/Detector.cs
+++ Assets/Scripts/Detector.cs
@@ -1,3 +1,4 @@
+using Fungus;
 using UnityEngine;
 
 public class Detector : MonoBehaviour
@@ -5,6 +6,15 @@ public class Detector : MonoBehaviour
     [SerializeField] private string objectTag = "Player";
     [HideInInspector] public bool isWithinCollider;
 
+    [SerializeField] public GameObject PanelB;
+    [SerializeField] public GameObject PanelL;
+    [SerializeField] public GameObject Text;
+    [SerializeField] public GameObject Name;
+    [SerializeField] public GameObject Image;
+    [SerializeField] public GameObject A;
+    [SerializeField] public GameObject B;
+    [SerializeField] public GameObject C;
+    [SerializeField] public GameObject Enemy;
 
     public GameObject flowChartObj;
 
@@ -38,8 +48,15 @@ public class Detector : MonoBehaviour
             else if (Input.GetKeyDown(KeyCode.E))
             {
                 Debug.Log("E pressed");
-                flowChartObj.SetActive(true);
-                patrolAI.Nspeed = 0f;
+                PanelB.SetActive(true);
+                PanelL.SetActive(true);
+                Text.SetActive(true);
+                Name.SetActive(true);
+                Image.SetActive(true);
+                A.SetActive(true);
+                B.SetActive(true);
+                C.SetActive(true);
+                Enemy.SetActive(true);
             }
         }
     }
