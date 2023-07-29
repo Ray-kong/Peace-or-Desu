994474ca2ae83b5430bc7ce56f98e063ca632cec
Keito605 <82559995+Keito605@users.noreply.github.com>
2023-07-30 06:17:58 +0900

    ee

--- Assets/Scripts/Detector.cs
+++ Assets/Scripts/Detector.cs
@@ -16,9 +16,15 @@ public class Detector : MonoBehaviour
     [SerializeField] public GameObject TextAnswer3;
     [SerializeField] public GameObject Name;
     [SerializeField] public GameObject Image;
-    [SerializeField] public GameObject A;
-    [SerializeField] public GameObject B;
-    [SerializeField] public GameObject C;
+    [SerializeField] public GameObject A1;
+    [SerializeField] public GameObject A2;
+    [SerializeField] public GameObject A3;
+    [SerializeField] public GameObject B1;
+    [SerializeField] public GameObject B2;
+    [SerializeField] public GameObject B3;
+    [SerializeField] public GameObject C1;
+    [SerializeField] public GameObject C2;
+    [SerializeField] public GameObject C3;
     [SerializeField] public GameObject NextButton;
     [SerializeField] public GameObject End;
     [SerializeField] public GameObject Enemy;
@@ -62,9 +68,9 @@ public class Detector : MonoBehaviour
                 Text1.SetActive(true);
                 Name.SetActive(true);
                 Image.SetActive(true);
-                A.SetActive(true);
-                B.SetActive(true);
-                C.SetActive(true);
+                A1.SetActive(true);
+                B1.SetActive(true);
+                C1.SetActive(true);
                 Enemy.SetActive(true);
                 textcount = 1;
             }
@@ -73,27 +79,27 @@ public class Detector : MonoBehaviour
 
     public void OK1()
     {
-        A.SetActive(false);
-        B.SetActive(false);
-        C.SetActive(false);
+        A1.SetActive(false);
+        B1.SetActive(false);
+        C1.SetActive(false);
         Text1.SetActive(false);
         TextAnswer1.SetActive(true);
         NextButton.SetActive(true);
     }
     public void OK2()
     {
-        A.SetActive(false);
-        B.SetActive(false);
-        C.SetActive(false);
+        A2.SetActive(false);
+        B2.SetActive(false);
+        C2.SetActive(false);
         Text2.SetActive(false);
         TextAnswer2.SetActive(true);
         NextButton.SetActive(true);
     }
     public void OK3()
     {
-        A.SetActive(false);
-        B.SetActive(false);
-        C.SetActive(false);
+        A3.SetActive(false);
+        B3.SetActive(false);
+        C3.SetActive(false);
         Text3.SetActive(false);
         TextAnswer3.SetActive(true);
         End.SetActive(true);
@@ -101,17 +107,79 @@ public class Detector : MonoBehaviour
     //äOÇµÇΩèÍçá
     public void NO1()
     {
-        A.SetActive(false);
-        B.SetActive(false);
-        C.SetActive(false);
+        A1.SetActive(false);
+        B1.SetActive(false);
+        C1.SetActive(false);
         Text1.SetActive(false);
         TextAnswer1.SetActive(true);
+        End.SetActive(true);
+    }
+    public void NO2()
+    {
+        A2.SetActive(false);
+        B2.SetActive(false);
+        C2.SetActive(false);
+        Text2.SetActive(false);
+        TextAnswer2.SetActive(true);
+        End.SetActive(true);
+    }
+    public void NO3()
+    {
+        A3.SetActive(false);
+        B3.SetActive(false);
+        C3.SetActive(false);
+        Text3.SetActive(false);
+        TextAnswer3.SetActive(true);
         End.SetActive(true);
     }
     //ñ‚ëËâÊñ Ç™è¡Ç¶ÇÈ
     public void EndButton()
     {
-
+        PanelB.SetActive(false);
+         PanelL.SetActive(false);
+        Text1.SetActive(false);
+        Text2.SetActive(false);
+        Text3.SetActive(false);
+        TextAnswer1.SetActive(false);
+         TextAnswer2.SetActive(false);
+        TextAnswer3.SetActive(false);
+        Name.SetActive(false);
+        Image.SetActive(false);
+        A1.SetActive(false);
+        B1.SetActive(false);
+        C1.SetActive(false);
+        A2.SetActive(false);
+        B2.SetActive(false);
+        C2.SetActive(false);
+        A3.SetActive(false);
+        B3.SetActive(false);
+        C3.SetActive(false);
+        NextButton.SetActive(false);
+        End.SetActive(false);
+        Enemy.SetActive(false);
     }
+
+    public void NextButton12()
+
+    {
+        A2.SetActive(true);
+        B2.SetActive(true);
+        C2.SetActive(true);
+        TextAnswer1.SetActive(false);
+        Text2.SetActive(true);
+        NextButton.SetActive(false);
+    }
+
+    public void NextButton23()
+
+    {
+        A3.SetActive(true);
+        B3.SetActive(true);
+        C3.SetActive(true);
+        TextAnswer2.SetActive(false);
+        Text3.SetActive(true);
+        NextButton.SetActive(false);
+    }
+
 
 }
